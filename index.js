0e03b8c5df380e9fd7e2bddb0f85d5a264961b90
varshith-goud <146644681+varshith-goud@users.noreply.github.com>
2024-08-27 21:00:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1=Math.floor(Math.random() * 6)+1;
+var randomDiceImage ="dice"+randomNumber1+".png";
+var randomImageSource="images/"+ randomDiceImage;
+var image1 =document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+var randomNumber2=Math.floor(Math.random() * 6)+1;
+var randomDiceImagee ="dice"+randomNumber2+".png";
+var randomImageSourcee="images/"+ randomDiceImagee;
+var image2 =document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSourcee);
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 wins";
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML="Player 2 wins";
+}
+else{
+    document.querySelector("h1").innerHTML="draw!"; 
+}
